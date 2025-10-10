4e0f3703d6909bd136ca00b0279a28d836ab7d29
Aditya Raj <aditya.raj@perficient.com>
2025-10-10 19:18:20 +0530

    Added logo

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -2,10 +2,21 @@ import React, { useState } from 'react';
 import Dashboard from './Dashboard';
 import SingleExpense from './SingleExpense';
 import GroupsPage from './GroupsPage';
+import Lottie from 'react-lottie';
+import Wallet from '../Wallet.json';
 
 function MainApp({ onLogout }) {
   // State to manage which view is currently active
   const [activeView, setActiveView] = useState('dashboard');
+
+  const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: Wallet, // The animation data goes here
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
 
   // Function to render the correct component based on the active view
   const renderView = () => {
@@ -24,7 +35,10 @@ function MainApp({ onLogout }) {
   return (
     <div className="p-5 mt-5 flex flex-col gap-5">
       <header className="flex flex-row outline-2 outline-red-100 p-2 rounded-xl bg-white">
-        <h1 className='basis-1/3 font-bold text-5xl'>PocketPal</h1>
+        <div className='basis-1/3 flex flex-row'>
+        <div className='basis-1/2 font-bold text-5xl flex justify-center items-center  '>PocketPal</div>
+        <div className='basis-1/2'><Lottie options={defaultOptions} height={75} width={75}/></div>
+        </div>
         
         <nav className='basis-1/3 flex flex-row gap-4'>
           <button className='basis-1/3' onClick={() => setActiveView('dashboard')}>Dashboard</button>
